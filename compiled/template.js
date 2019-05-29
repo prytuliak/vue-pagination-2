4dacba0e167a8bccf9e4d5d45086df7bf4da1fd8
Bohdan <meandyou2903@gmail.com>
2019-05-29 15:13:39 +0300

    added three dots functionality to pagination

--- compiled/template.js
+++ compiled/template.js
@@ -10,8 +10,9 @@ module.exports = function () {
     var nextChunk = '';
     var firstPage = '';
     var lastPage = '';
-
+    
     if (this.opts.edgeNavigation && this.totalChunks >= 1) {
+      
       firstPage = h(
         'li',
         { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + (this.page === 1 ? theme.disabled : '') + ' VuePagination__pagination-item-prev-chunk' },
@@ -83,26 +84,232 @@ module.exports = function () {
         )]
       );
     }
-
-    this.pages.map(function (page) {
-      items.push(h(
-        'li',
-        { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + this.activeClass(page) },
-        [h(
-          'a',
-          { 'class': theme.link + ' ' + this.activeClass(page),
-            attrs: { href: 'javascript:void(0)',
-              role: 'button'
+    if (this.totalPages <= 10) {
+      for (var i = 1; i <= this.totalPages; i++) {
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + this.activeClass(i) },
+          [h(
+            'a',
+            {
+              'class': theme.link + ' ' + this.activeClass(i),
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              },
+              on: {
+                'click': this.setPage.bind(this, i)
+              }
             },
-            on: {
-              'click': this.setPage.bind(this, page)
-            }
-          },
-          [page]
-        )]
-      ));
-    }.bind(this));
+            [i]
+          )]
+        ));
+      }
+    }
+    if (this.totalPages > 10) {
+      if (this.page < 7) {
+        for (var i = 1; i <= 8; i++) {
+          items.push(h(
+            'li',
+            { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + this.activeClass(i) },
+            [h(
+              'a',
+              {
+                'class': theme.link + ' ' + this.activeClass(i),
+                attrs: {
+                  href: 'javascript:void(0)',
+                  role: 'button'
+                },
+                on: {
+                  'click': this.setPage.bind(this, i)
+                }
+              },
+              [i]
+            )]
+          ));
+        }
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              }
+            },
+            ['...']
+          )]
+        ));
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' + theme.item },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              },
+              on: {
+                'click': this.setPage.bind(this, this.totalPages)
+              }
+            },
+            [this.totalPages]
+          )]
+        ));
 
+      }
+      if (this.page >= 7 && this.page <= this.totalPages - 6) {
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' + theme.item },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              },
+              on: {
+                'click': this.setPage.bind(this, this.firstPage)
+              }
+            },
+            [this.firstPage]
+          )]
+        ));
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              }
+            },
+            ['...']
+          )]
+        ));
+
+        for (var i = this.page - 2; i <= this.page + 2; i++) {
+          items.push(h(
+            'li',
+            { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + this.activeClass(i) },
+            [h(
+              'a',
+              {
+                'class': theme.link + ' ' + this.activeClass(i),
+                attrs: {
+                  href: 'javascript:void(0)',
+                  role: 'button'
+                },
+                on: {
+                  'click': this.setPage.bind(this, i)
+                }
+              },
+              [i]
+            )]
+          ));
+        }
+
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              }
+            },
+            ['...']
+          )]
+        ));
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' + theme.item },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              },
+              on: {
+                'click': this.setPage.bind(this, this.totalPages)
+              }
+            },
+            [this.totalPages]
+          )]
+        ));
+      }
+      if (this.page > this.totalPages - 6) {
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' + theme.item },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              },
+              on: {
+                'click': this.setPage.bind(this, this.firstPage)
+              }
+            },
+            [this.firstPage]
+          )]
+        ));
+        items.push(h(
+          'li',
+          { 'class': 'VuePagination__pagination-item ' },
+          [h(
+            'a',
+            {
+              'class': theme.link,
+              attrs: {
+                href: 'javascript:void(0)',
+                role: 'button'
+              }
+            },
+            ['...']
+          )]
+        ));
+        for (var i = this.totalPages - 7; i <= this.totalPages; i++) {
+          items.push(h(
+            'li',
+            { 'class': 'VuePagination__pagination-item ' + theme.item + ' ' + this.activeClass(i) },
+            [h(
+              'a',
+              {
+                'class': theme.link + ' ' + this.activeClass(i),
+                attrs: {
+                  href: 'javascript:void(0)',
+                  role: 'button'
+                },
+                on: {
+                  'click': this.setPage.bind(this, i)
+                }
+              },
+              [i]
+            )]
+          ));
+        }
+      }
+    }
+    
     return h(
       'div',
       { 'class': 'VuePagination ' + theme.wrapper },
